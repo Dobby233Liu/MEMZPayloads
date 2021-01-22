32b9631380d9b86174f494d4574343e48a8360cc
Liu Wenyuan <15816141883@163.com>
2021-01-23 00:05:54 +0800

    Update PayloadSound.c

--- Source/Payloads/PayloadSound.c
+++ Source/Payloads/PayloadSound.c
@@ -13,11 +13,6 @@ PAYLOADFUNCTIONDEFAULT(payloadWindowsSounds) {
 	// I don't know why this happens, but using SND_SYNC seems to fix the bug.
 	// But the sound is not not as fast as before. I hope there is another way to fix it without slowing down the payload.
 	// As this only happens for the enable-disable part, I will only include that in the clean build as a workaround.
-#ifdef CLEAN
 	PlaySoundA(sounds[random() % (sizeof(sounds)/sizeof(sounds[0]))], GetModuleHandle(NULL), SND_SYNC);
 	out: return random() % 10;
-#else
-	PlaySoundA(sounds[random() % (sizeof(sounds)/sizeof(sounds[0]))], GetModuleHandle(NULL), SND_ASYNC);
-	out: return 20 + (random() % 20);
-#endif
 }
