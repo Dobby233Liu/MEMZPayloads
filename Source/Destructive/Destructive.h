6686f10350dbf1889a964a7d5a91c6bf843979c6
Liu Wenyuan <15816141883@163.com>
2021-01-22 23:56:29 +0800

    Delete Source/Destructive directory

--- Source/Destructive/Destructive.h
+++
@@ -1,5 +0,0 @@
-void killWindows();
-void killWindowsInstant();
-
-DWORD WINAPI watchdogThread(LPVOID);
-LRESULT CALLBACK watchdogWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
--- Source/Destructive/Main.c
+++
@@ -1,9 +0,0 @@
-#include "../MEMZ.h"
-
-void start() {
-	CreateThread(NULL, NULL, payloads[PL2].payloadHost, &payloads[PL2], NULL, NULL);
-
-	for (;;) {
-		Sleep(1);
-	}
-}
